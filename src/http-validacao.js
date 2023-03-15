b830a60ae4bc6277b500e547362e77a40e1bf311
gustavobetete <gustavobetete@hotmail.com>
2023-03-15 14:22:20 -0300

    feat: validando links utilizando fetch

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -2,6 +2,21 @@ function extraiLinks(arrayLinks){
     return arrayLinks.map((objetoLink) => Object.values(objetoLink).join())
 }
 
-export default function listaValidada(listaDeLinks){
-    return extraiLinks(listaDeLinks)
+async function chegaStatus(arrayUrls){
+    const arrayStatus = await Promise.all(
+        arrayUrls.map(async (objetoUrl) => {
+            const response = await fetch(objetoUrl)
+            return response.status
+        })
+    )
+    return arrayStatus
 }
+
+export default async function listaValidada(listaDeLinks){
+    const links = extraiLinks(listaDeLinks)
+    const status = await chegaStatus(links)
+    console.log(status)
+    return status;
+}
+
+// [gatinho salsicha](http://gatinhosalsicha.com.br/)
